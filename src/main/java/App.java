d96eab6738cf7e37b0acd96acfd854f24c918d72
1982moc <1982moc@gmail.com>
2024-02-05 23:29:46 +0100

    new commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,21 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHelloWorld1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(beanHelloWorld1.getMessage());
+
+        HelloWorld beanHelloWorld2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHelloWorld2.getMessage());
+        System.out.println("Сравнение HelloWorld " + (beanHelloWorld1 == beanHelloWorld2));
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Cat 1: " + beanCat1.getName());
+
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Сравнение CatBeans: " + (beanCat1 == beanCat2));
+
     }
 }
